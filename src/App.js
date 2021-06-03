a3a6e11b2077b3980a0ec1f33dc27401757581de
Pavel <recyclesordie94@gmail.com>
2021-06-03 21:07:26 +0300

    настройка окружение, переиспользование компонентов из старых проектов

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+import "./App.css";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
-  return ( 
-    <div className = "App" >
+  return (
+    <div className="content">
+      <Header />
 
-    hello read_config
+      <main className="container content">
+        <Router>
+<Route path='/about' component={About} />
+<Route path='/contact' component={Contact} />
+<Route path='/Home' component={Home} />
 
+        </Router>
+      </main>
 
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
                 <div className="footer-copyright">
                     <div className="container">
                         © {new Date().getFullYear()} Copyright Text
-                        <a className="grey-text text-lighten-4 right" href="https://github.com/PavelMelnik94/react-movies">Repo</a>
+                        <a className="grey-text text-lighten-4 right" href="https://github.com/PavelMelnik94/food-project">Repo</a>
                     </div>
                 </div>
             </footer>
